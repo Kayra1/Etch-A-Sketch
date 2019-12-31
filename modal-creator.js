703a9f0365d4a7ec6291c7e67f33472b6182e1cb
Kayra <kayra959@gmail.com>
2019-12-30 19:45:03 -0800

    cleaned up files

---
+++ modal-creator.js
@@ -0,0 +1,68 @@
+function launchModal(){
+    // Stop painting functionality
+
+    removeHoverListener();
+
+    // Create the original settings modal
+    let settings = document.createElement("div");
+    let darkBackground = document.createElement("div");
+    let closeModal = document.createElement("button");
+    let modalContent = document.createElement("div");
+
+    settings.setAttribute("class","modal is-active");
+    darkBackground.setAttribute("class","modal-background");
+    modalContent.setAttribute("class", "modal-content");
+    closeModal.setAttribute("class", "modal-close is-large");
+    closeModal.setAttribute("onclick", "removeModal()");
+
+    // Append background first
+    settings.appendChild(darkBackground);
+
+    // Create the content
+    let settingsBox = document.createElement("div");
+    let settingsForm = document.createElement("form");
+    settingsBox.setAttribute("class", "box");
+
+
+    // Grid Size
+    let gridSizeQ = document.createElement("p");
+    gridSizeQ.innerText = "What is your preferred grid size?";
+    let gridSizeInput = document.createElement("input");
+    gridSizeInput.setAttribute("class", "input is-rounded");
+    gridSizeInput.setAttribute("id", "grid-size");
+    gridSizeInput.setAttribute("type", "number");
+    gridSizeInput.setAttribute("placeholder", "16");
+
+
+    // Append these settings to the content div
+    settingsBox.appendChild(gridSizeQ);
+    settingsBox.appendChild(gridSizeInput);
+
+    modalContent.appendChild(settingsBox);
+
+    // Append the content div to the settings modal second
+    settings.appendChild(modalContent);
+
+    // Append close button to the end
+    settings.appendChild(closeModal);
+
+    // Add this modal to the root of the grid
+    gridHead = document.getElementById("grid-container");
+    gridHead.appendChild(settings);
+}
+
+function removeModal() {
+    // Save Settings
+    let gridSize = document.getElementById('grid-size').value;
+    let gridHead = document.getElementById('grid-container');
+    gridHead.setAttribute('gridsize', `${gridSize}`);
+
+    // Find the modal (there will always be only one)
+    mountedModal = document.getElementsByClassName("modal is-active");
+    
+    // Remove it
+    mountedModal[0].remove();
+
+    // Restart painting mode
+    addHoverListener();
+}
